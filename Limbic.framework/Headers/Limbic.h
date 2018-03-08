4cc97c31e03474841422c783563c2f5ee11da93a
Bas de Vries <mail@basdevri.es>
2018-03-08 15:10:18 +0000

    0.0.9

--- Limbic.framework/Headers/Limbic.h
+++ Limbic.framework/Headers/Limbic.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double LimbicVersionNumber;
 FOUNDATION_EXPORT const unsigned char LimbicVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Limbic/PublicHeader.h>
-
-
